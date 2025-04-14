2fc36b65da82c7c3d2eb0e30bd54d50ff285188d
anjumArnab <anjumarnab050@gmail.com>
2025-04-15 00:38:01 +0600

    hive initiliazation

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,24 @@
 import 'package:dbapp/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
-  runApp(const DBApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  await Hive.openBox('todosBox');
+  runApp(const ToDo());
 }
 
-class DBApp extends StatelessWidget {
-  const DBApp({super.key});
+class ToDo extends StatelessWidget {
+  const ToDo({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Todo List",
-      theme: ThemeData(
-        textTheme: GoogleFonts.poppinsTextTheme()
-      ),
+      theme: ThemeData(textTheme: GoogleFonts.poppinsTextTheme()),
       home: const HomeScreen(),
     );
   }
