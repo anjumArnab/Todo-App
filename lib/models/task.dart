f39c5ecaa5b6723c0421d27433c1897d67a04af4
anjumArnab <anjumarnab050@gmail.com>
2025-04-15 18:42:42 +0600

    ig:

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -7,25 +7,25 @@ part 'task.g.dart'; // Run build_runner to generate the adapter
 class Task {
   @HiveField(0)
   int? id;
-
+  
   @HiveField(1)
   String title;
-
+  
   @HiveField(2)
   String? description;
-
+  
   @HiveField(3)
   String dueDate;
-
+  
   @HiveField(4)
   String dueTime;
-
+  
   @HiveField(5)
   String priority;
-
+  
   @HiveField(6)
   List<SubTask> subTasks;
-
+  
   Task({
     this.id,
     required this.title,
