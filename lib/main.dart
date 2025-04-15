071180ee0f5156af6c89f1afcd6df329ae327647
anjumArnab <anjumarnab050@gmail.com>
2025-04-16 02:39:55 +0600

    supabase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,26 @@
+import 'package:dbapp/screens/home_screen.dart';
+import 'package:dbapp/supa_secret.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'models/task.dart';
 import 'models/subtask.dart';
-import 'screens/home_screen.dart';
 
 void main() async {
+  // Initialize Supabase
+  await Supabase.initialize(
+    url: url,
+    anonKey: anonKey,
+  );
+
   // Initialize Hive
   await Hive.initFlutter();
-  
+
   // Register adapters
   Hive.registerAdapter(TaskAdapter());
   Hive.registerAdapter(SubTaskAdapter());
-  
+
   // Now we can run the app
   runApp(const ToDo());
 }
@@ -20,11 +28,11 @@ void main() async {
 class ToDo extends StatelessWidget {
   const ToDo({super.key});
 
-   @override
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Todo List",
+      title: 'Taskio',
       theme: ThemeData(
         textTheme: GoogleFonts.poppinsTextTheme(),
       ),
