fc87579b0242ff65b4f49b957abc329f6879a230
anjumArnab <anjumarnab050@gmail.com>
2025-04-15 18:42:07 +0600

    hive initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,33 @@
-import 'package:dbapp/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'models/task.dart';
+import 'models/subtask.dart';
+import 'screens/home_screen.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  // Initialize Hive
   await Hive.initFlutter();
-  await Hive.openBox('todosBox');
+  
+  // Register adapters
+  Hive.registerAdapter(TaskAdapter());
+  Hive.registerAdapter(SubTaskAdapter());
+  
+  // Now we can run the app
   runApp(const ToDo());
 }
 
 class ToDo extends StatelessWidget {
   const ToDo({super.key});
 
-  @override
+   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Todo List",
-      theme: ThemeData(textTheme: GoogleFonts.poppinsTextTheme()),
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(),
+      ),
       home: const HomeScreen(),
     );
   }
